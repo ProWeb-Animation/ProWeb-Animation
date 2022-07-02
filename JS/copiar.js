5bef378145fc0b84955baaec53e9f3d67aa412f3
ProWeb-Animation <108555962+ProWeb-Animation@users.noreply.github.com>
2022-07-01 21:15:45 -0500

    Add files via upload

---
+++ JS/copiar.js
@@ -0,0 +1,9 @@
+function copyToClipBoard() {
+
+    var content = document.getElementById('ff');
+    
+    content.select();
+    document.execCommand('proweb_animation@outlook.com');
+
+    alert("Copiado!");
+}
