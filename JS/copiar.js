95fb681f4367d9bcd6e83afacb50a41e90c618fd
ProWeb-Animation <108555962+ProWeb-Animation@users.noreply.github.com>
2022-07-05 18:03:29 -0500

    Delete JS directory

--- JS/copiar.js
+++
@@ -1,9 +0,0 @@
-function copyToClipBoard() {
-
-    var content = document.getElementById('ff');
-    
-    content.select();
-    document.execCommand('proweb_animation@outlook.com');
-
-    alert("Copiado!");
-}
